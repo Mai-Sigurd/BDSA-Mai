880ad961cb66cfe3dc5f3ba851f0305bfac4573e
Mai Sigurd Adelheid Odgaard <63573363+Mai-sao@users.noreply.github.com>
2021-09-06 17:39:08 +0200

    Created user interface for IsLeapYear. (#3)
    * Tested and implemented IsLeapYear
    
    * avoided static methods
    
    * Created user interface for IsLeapYear.

--- LeapYear.Tests/ProgramTests.cs
+++ LeapYear.Tests/ProgramTests.cs
@@ -1,13 +1,16 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace LeapYear.Tests
 {
-    public class UnitTest1
+    public class ProgramTests
     {
+        
         [Fact]
         public void divisble_by_four()
         {
+
             Assert.True(Program.isLeapYear(2004));
             Assert.False(Program.isLeapYear(1999));
         }
@@ -17,6 +20,36 @@ namespace LeapYear.Tests
         {
             Assert.True(Program.isLeapYear(2000));
             Assert.False(Program.isLeapYear(1900));
+        }
+
+        [Fact]
+        public void prints_yay()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            // Act
+            Program.printLeapYear(1996);
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            // Assert
+            Assert.Equal("yay", output);
+        }
+
+        [Fact]
+        public void prints_nay()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            // Act
+            Program.printLeapYear(1800);
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            // Assert
+            Assert.Equal("nay", output);
         }
     }
 }
