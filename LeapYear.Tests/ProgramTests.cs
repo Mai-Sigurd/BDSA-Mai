80a637841bbf7f284c1abf79477f4dd74a9cdba6
SamsoeT <odgaardmai@gmail.com>
2021-09-06 17:32:22 +0200

    Created user interface for IsLeapYear.

--- LeapYear.Tests/ProgramTests.cs
+++ LeapYear.Tests/ProgramTests.cs
@@ -1,24 +1,55 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace LeapYear.Tests
 {
-    public class UnitTest1
+    public class ProgramTests
     {
-        Program program = new Program();
+        
         [Fact]
         public void divisble_by_four()
         {
-            
-            Assert.True(program.isLeapYear(2004));
-            Assert.False(program.isLeapYear(1999));
+
+            Assert.True(Program.isLeapYear(2004));
+            Assert.False(Program.isLeapYear(1999));
         }
 
         [Fact]
         public void centurial_leap_years()
         {
-            Assert.True(program.isLeapYear(2000));
-            Assert.False(program.isLeapYear(1900));
+            Assert.True(Program.isLeapYear(2000));
+            Assert.False(Program.isLeapYear(1900));
+        }
+
+        [Fact]
+        public void prints_yay()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            // Act
+            Program.printLeapYear(1996);
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            // Assert
+            Assert.Equal("yay", output);
+        }
+
+        [Fact]
+        public void prints_nay()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            // Act
+            Program.printLeapYear(1800);
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            // Assert
+            Assert.Equal("nay", output);
         }
     }
 }
--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -4,18 +4,47 @@ namespace LeapYear
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            leapYearProgram();
         }
 
-      
-        public  bool isLeapYear(int year){
-            if(year % 4 == 0){
-                if(year % 100 ==0){
-                    return year % 400 ==0;
-                } else return true;
-            } else return false;
+        public static void leapYearProgram()
+        {
+            printLeapYear(getUserInput());
+        }
+
+        public static int getUserInput()
+        {
+            var userInput = Console.ReadLine();
+            int intVal = Convert.ToInt32(userInput);
+            return intVal;
+        }
+
+        public static void printLeapYear(int year)
+        {
+            string statement = new string("");
+            if (isLeapYear(year))
+            {
+                statement = "yay";
+            }
+            else
+            {
+                statement = "nay";
+            }
+            Console.WriteLine(statement);
+        }
+        public static bool isLeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    return year % 400 == 0;
+                }
+                else return true;
+            }
+            else return false;
         }
 
 
