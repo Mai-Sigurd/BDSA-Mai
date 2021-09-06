53e33f70597672740b5cb963bac8c30d6dbf0730
SamsoeT <odgaardmai@gmail.com>
2021-09-06 17:03:20 +0200

    avoided static methods

--- LeapYear.Tests/ProgramTests.cs
+++ LeapYear.Tests/ProgramTests.cs
@@ -5,18 +5,20 @@ namespace LeapYear.Tests
 {
     public class UnitTest1
     {
+        Program program = new Program();
         [Fact]
         public void divisble_by_four()
         {
-            Assert.True(Program.isLeapYear(2004));
-            Assert.False(Program.isLeapYear(1999));
+            
+            Assert.True(program.isLeapYear(2004));
+            Assert.False(program.isLeapYear(1999));
         }
 
         [Fact]
         public void centurial_leap_years()
         {
-            Assert.True(Program.isLeapYear(2000));
-            Assert.False(Program.isLeapYear(1900));
+            Assert.True(program.isLeapYear(2000));
+            Assert.False(program.isLeapYear(1900));
         }
     }
 }
--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -9,12 +9,15 @@ namespace LeapYear
             
         }
 
-        public static bool isLeapYear(int year){
+      
+        public  bool isLeapYear(int year){
             if(year % 4 == 0){
                 if(year % 100 ==0){
                     return year % 400 ==0;
                 } else return true;
             } else return false;
         }
+
+
     }
 }
